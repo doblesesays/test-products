600530f653cdb9eac0216f4c09e398fa34b3cc5d
Genessis Jimenez <genesis.jz.93@gmail.com>
2018-02-03 15:22:49 -0400

    primer commit - empezando signin screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//material imports
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatToolbarModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/singin/signin-screen.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-signin-screen',
+    template: 'vista de signin'
+})
+
+export class SigninScreenComponent {}
